c202f3a571795fb65a534369a248278d243f0141
bubbleman3333 <keizaburo.takashiba@gmail.com>
2023-05-07 15:13:01 +0900

    追加

--- src/自然言語学習/train/train.py
+++ src/自然言語学習/train/train.py
@@ -4,3 +4,10 @@ import pickle
 from src.ニューラルネットワークの練習.クラス一覧 import Adam, CBOw, to_gpu, to_cpu
 from src.dataset import ptb
 from src.自然言語学習.common.trainer import Trainer
+
+window_size = 5
+hidden_size = 100
+batch_size = 100
+max_epoch = 10
+corpus, word_to_id, id_to_word = ptb.load_data("train")
+vocab_size = len(word_to_id)
